589ad3c4ab7b576390d8931860c6c29d05b0c830
Jasal Vadgama <jasal@blacklabelcreative.com>
2014-11-18 22:58:43 +0000

    adding css back in

--- site/wp-content/themes/jamesnighthawk/parts/shared/footer.php
+++ site/wp-content/themes/jamesnighthawk/parts/shared/footer.php
@@ -11,7 +11,7 @@
             </nav><!-- #access -->
 
             <ul class="music-links">
-                <li><a href="https://itunes.apple.com/gb/artist/james-nighthawk/id603994693"><img alt="Download on iTunes" src="/wp-content/themes/jamesnighthawk/images/download-on-itunes.png" /></a></li>
+                <li><a href="https://itunes.apple.com/gb/artist/james-nighthawk/id603994693"><img alt="Download on iTunes" src="/wp-content/themes/jamesnighthawk/assets/images/download-on-itunes.png" /></a></li>
             </ul>
 
             <p class="copyright">&copy; 2013 James Nighthawk</p>
